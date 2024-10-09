b496c63a62b10c076f1ba6e8ab6d155039ff8ea7
ehdtndla123 <ehdtndla123@naver.com>
2024-10-10 01:05:56 +0900

    refactor : 주요 기능 database_manager로 분리

--- trading-engine/app/db/database.py
+++ trading-engine/app/db/database.py
@@ -1,29 +1,20 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-from app.core.config import settings
-
-engine = create_engine(settings.DATABASE_URL)
-session_factory = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-SessionLocal = scoped_session(session_factory=session_factory)
-Base = declarative_base()
+from app.db.database_manager import db_manager
+from sqlalchemy.orm import Session
 
 
-def get_db():
-    db = SessionLocal()
+def get_db() -> Session:
+    db = next(db_manager.get_db())
     try:
         yield db
     finally:
         db.close()
 
 
-def init_db():
-    Base.metadata.drop_all(bind=engine)
-    print("Existing tables dropped.")
-    Base.metadata.create_all(bind=engine)
-    print("Database tables created.")
-
-
 def initialize_database():
-    init_db()
-    print("Database initialized successfully.")
+    db_manager.initialize_database()
+
+
+Base = db_manager.Base
+tunnel = db_manager.tunnel
+SessionLocal = db_manager.SessionLocal
+engine = db_manager.engine
